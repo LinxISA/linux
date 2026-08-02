f087e31d925f5ca896c2a23d1de91668c4a1c9be
RuoyuZhou <ruoyu.zhou@hisilicon.com>
2026-08-02 20:04:23 +0800

    Bind Linux loader to merged PTO ISA 0.57.1

--- fs/binfmt_elf.c
+++ fs/binfmt_elf.c
@@ -78,7 +78,7 @@
 static const char linx_pto_isa_identity[] =
 	"{\"encoding_abi\":\"pto-isa-0.57.1-mode-function-v1\","
 	"\"encoding_projection_sha256\":"
-	"\"34f6602cf29ea6363d41d896111dad4de0f70ec36517138aa89e292857909da4\","
+	"\"9705a984e2e48e0d4e856d3fbcfa07041c8578dd326d81f1c90279e826354c32\","
 	"\"release\":\"0.57.1\"}";
 
 #define LINX_PTO_ISA_NOTE_SCAN_MAX SZ_4K
